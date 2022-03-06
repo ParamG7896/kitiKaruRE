57ae3ffa15985d7d941702c450231fcd6d0d5fd8
ParamG7896 <psir7896@gmail.com>
2022-03-06 14:59:48 +0530

    first commit
    first commit1

---
+++ Aarry1.java
@@ -0,0 +1,21 @@
+package actions1;
+
+import java.util.ArrayList;
+
+public class Aarry1 {
+	public static String e = "33,45";
+	static public  void main (String [] args) {
+		ArrayList<Integer> l = new ArrayList<Integer>();
+		l.add(10);
+		l.add(20);
+		l.add(10);
+		l.add(80);
+		l.add(10);
+		int a = l.indexOf(10);
+		System.out.println(a);
+	
+	}
+	static {System.out.println("This is me");}
+	{System.out.println("This is me2");}
+
+}
